e326a01ee2e12281bd555e4a99e1ea5b92280839
menzDev <chetomasrandom@gmail.com>
2023-12-19 15:38:58 -0500

    build: :fire:

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown'
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +14,15 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), sitemap()],
+  }), 
+  sitemap(),
+  partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),
+  react(),
+],
   output: 'server',
   adapter: vercel()
 });
